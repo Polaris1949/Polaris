40429664c55b948ffc0877b2da6a224f276a96a4
Polaris1949 <Polaris1949@126.com>
2018-07-06 17:25:36 +0800

    Update test for byte stream

--- testsuite/pol-byte_stream.cpp
+++ testsuite/pol-byte_stream.cpp
@@ -4,18 +4,28 @@
 using namespace std;
 using namespace polaris;
 
+byte_stream<ifstream> bin;
+byte_stream<ofstream> bout;
+
 int main()
 {
-	byte_stream<ofstream> bout;
+	unsigned x{}, y{};
+	char c[20]="ABCDE";
+	c[6]='F';c[7]='G';c[8]='H';c[9]='\0';
+
 	bout.open("test-byte_stream.txt", ios_base::out|ios_base::trunc);
-	char a='x';
-	bout << a << '\n';
+	x=3242398579u; y=2811149670u;
+	bout << x << y << c << '\0' << c+6;
 	bout.close();
-	byte_stream<ifstream> bin;
+
 	bin.open("test-byte_stream.txt", ios_base::in);
-	a='y';
-	cout << a << endl;
-	bin >> a;
-	cout << a << endl;
+	bin >> x >> y;
+	cout << x << ' ' << y << endl;
+	char d[20];
+	bin >> d;
+	cout << d << endl;
+	bin >> d;
+	cout << d << endl;
+
 	return 0;
 }
