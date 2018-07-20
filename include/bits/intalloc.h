5483f9b2b56f541615713e60953dbb4ba044aaca
Polaris1949 <Polaris1949@126.com>
2018-07-20 18:13:48 +0800

    Update comment

--- include/bits/intalloc.h
+++ include/bits/intalloc.h
@@ -6,37 +6,62 @@
 namespace polaris
 {
 
-// @integer_allocator
+/** @class  integer_allocator
+ *  @headerfile  integer
+ *  @brief  Integer allocator abstract base class
+**/
 class integer_allocator
 {
 public:
-	typedef size_t  size_type;
-	typedef void*   pointer;
-	
+	typedef size_t  size_type;  ///< Size type
+	typedef void*   pointer;    ///< Pointer type
+
 protected:
 	// 2.15 allocator constructors and destructor
+	/** @brief  Construct a @c integer_allocator
+	**/
 	integer_allocator();
+
+	/** @brief  Destroy a @c integer_allocator
+	**/
 	virtual ~integer_allocator();
-	
+
 public:
 	// 2.16 allocator member functions
+	/** @brief  Allocate memory
+	 *  @param  __n  Size of memory.
+	 *  @return  A pointer.
+	**/
 	virtual pointer allocate(size_type __n) = 0;
+
+	/** @brief  Reallocate memory
+	 *  @param  __p  Pointer.
+	 *  @param  __n  Size of memory.
+	 *  @return  A new pointer.
+	**/
 	virtual pointer reallocate(pointer __p, size_type __n) = 0;
+
+	/** @brief  Deallocate memory
+	 *  @param  __p  Pointer.
+	**/
 	virtual void deallocate(pointer __p) = 0;
 };
 
-// integer default allocator class
+/** @class  _Int_allocator
+ *  @headerfile  integer
+ *  @brief  Integer default allocator class
+**/
 struct _Int_allocator : public integer_allocator
 {
 public:
 	_Int_allocator() noexcept;
-	
+
 	~_Int_allocator() noexcept;
-	
+
 	pointer allocate(size_type __n);
-	
+
 	pointer reallocate(pointer __p, size_type __n);
-	
+
 	void deallocate(pointer __p);
 };
 
