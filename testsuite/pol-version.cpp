b120a275891fcc6d29a6dfc964455662dd54997c
Polaris1949 <Polaris1949@126.com>
2018-09-14 22:46:26 +0800

    Update version

--- testsuite/pol-version.cpp
+++ testsuite/pol-version.cpp
@@ -1,13 +1,10 @@
-#define POL_DEPRECATED 1
 #include <iostream>
 #include <polaris/version>
-using namespace std;
-using namespace polaris;
 
 int main()
 {
-	cout << __POLLIB__ << '.' << __POLLIB_MINOR__ \
-		<< '.' << __POLLIB_PATCHLEVEL__ << endl;
-	cout << __cplusplus << endl;
+	std::cout << __POLLIB__ << '.' << __POLLIB_MINOR__ \
+		<< '.' << __POLLIB_PATCHLEVEL__ << '\n';
+	std::cout << __cplusplus << '\n';
 	return 0;
 }
