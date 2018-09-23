4c9f1701c2d38fa29b44bd3ef3b0af67e9bf1d9b
Polaris1949 <Polaris1949@126.com>
2018-09-23 19:32:30 +0800

    Tab format

--- testsuite/pol-chtree.cpp
+++ testsuite/pol-chtree.cpp
@@ -9,9 +9,9 @@ llong seed, mod;
 
 int rnd()
 {
-	int res = seed;
-	seed = (seed * 7 + 13) % 1000000007;
-	return res;
+    int res = seed;
+    seed = (seed * 7 + 13) % 1000000007;
+    return res;
 }
 
 int main()
@@ -27,38 +27,38 @@ int main()
         ct.init(data);
     }
 
-	while (m--)
-	{
-		int op{rnd() % 4 + 1};
-		int l{rnd() % n + 1};
-		int r{rnd() % n + 1};
-		int x{}, y{};
+    while (m--)
+    {
+        int op{rnd() % 4 + 1};
+        int l{rnd() % n + 1};
+        int r{rnd() % n + 1};
+        int x{}, y{};
 
-		if (l > r) std::swap(l, r);
-		if (op == 3) x = (rnd() % (r - l + 1)) + 1;
-		else x = (rnd() % mod) + 1;
-		if (op == 4) y = (rnd() % mod) + 1;
+        if (l > r) std::swap(l, r);
+        if (op == 3) x = (rnd() % (r - l + 1)) + 1;
+        else x = (rnd() % mod) + 1;
+        if (op == 4) y = (rnd() % mod) + 1;
         --l;
 
-		switch (op)
-		{
-		case 1:
+        switch (op)
+        {
+        case 1:
             ct.plus(l, r, x);
-			break;
+            break;
 
-		case 2:
+        case 2:
             ct.assign(l, r, x);
-			break;
+            break;
 
-		case 3:
+        case 3:
             std::cout << ct.rank(l, r, --x) << '\n';
-			break;
+            break;
 
-		case 4:
+        case 4:
             std::cout << ct.pow_sum(l, r, x, y) << '\n';
-			break;
-		}
-	}
+            break;
+        }
+    }
 
     return 0;
 }
