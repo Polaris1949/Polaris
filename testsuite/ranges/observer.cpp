c5a1b6e537f23af61f4cf416328a46b13a33c578
Polaris1949 <Polaris1949@126.com>
2018-10-08 21:32:13 +0800

    Update test

--- testsuite/ranges/observer.cpp
+++ testsuite/ranges/observer.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <polaris/range_observer>
-using namespace std;
-using namespace polaris;
 
-vector<int> v;
+std::vector<int> v;
 
 int main()
 {
 	for (int i=0; i<10; ++i)
 		v.push_back(i);
-	
-	range_observer<decltype(v)> ro(v, -5);
-	cout<<ro[10]<<endl;
+
+	pol::range_observer ro{v, -5};
+	std::cout << ro[10] << '\n';
 	return 0;
 }
