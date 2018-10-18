83069ca49a70d57cc594824ccc276cd6b5c2626c
Polaris1949 <Polaris1949@126.com>
2018-10-18 18:37:16 +0800

    Update test

--- testsuite/fundamental/boolean.cpp
+++ testsuite/fundamental/boolean.cpp
@@ -1,26 +1,10 @@
 #include <iostream>
 #include <polaris/boolean>
-using namespace std;
-using namespace polaris;
-
-void f(bool __x)
-{
-	cout << "f(bool)" << endl;
-}
-
-void f(int __x)
-{
-	cout << "f(int)" << endl;
-}
 
 int main()
 {
-	int x=0;
-	boolean b;
-	cout << b << endl;
-	cout << sizeof(b) << endl;
-	if (b)
-	{
-		cout << b << endl;
-	}
+	bool x{true};
+	pol::boolean b{x};
+	std::cout << b << '\n';
+	std::cout << sizeof(b) << '\n';
 }
