977a90be6921e39b1edd37be28d6a155030f4c20
Polaris1949 <Polaris1949@126.com>
2018-10-18 22:21:07 +0800

    Update concepts library

--- testsuite/concepts/concepts.cpp
+++ testsuite/concepts/concepts.cpp
@@ -1,6 +1,11 @@
 #include <polaris/concepts>
 
+class foo {};
+class bar : public foo {};
+
 int main()
 {
+    static_assert(std::is_same_v<pol::common_reference_t<int, char>, int>);
+    // static_assert(std::is_same_v<pol::common_reference_t<foo&, bar&>, foo&>);
     return 0;
 }
