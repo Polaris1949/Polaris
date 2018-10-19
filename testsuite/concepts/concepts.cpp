979c172a3bde3b003ff8a01fec3aa5452c5f14ec
Polaris1949 <Polaris1949@126.com>
2018-10-19 21:06:44 +0800

    Update concepts library

--- testsuite/concepts/concepts.cpp
+++ testsuite/concepts/concepts.cpp
@@ -5,7 +5,19 @@ class bar : public foo {};
 
 int main()
 {
-    static_assert(std::is_same_v<pol::common_reference_t<int, char>, int>);
-    // static_assert(std::is_same_v<pol::common_reference_t<foo&, bar&>, foo&>);
+    static_assert(std::is_same_v<pol::
+        __qualify_const_t<const foo, bar>, const bar>);
+    static_assert(std::is_same_v<pol::
+        __qualify_volatile_t<volatile foo, bar>, volatile bar>);
+    static_assert(std::is_same_v<pol::
+        __qualify_cv_t<const volatile foo, bar>, const volatile bar>);
+    static_assert(std::is_same_v<pol::
+        __qualify_lvalue_reference_t<foo&, const bar>, const bar&>);
+    static_assert(std::is_same_v<pol::
+        __qualify_rvalue_reference_t<foo&&, bar>, bar&&>);
+    static_assert(std::is_same_v<pol::
+        __qualify_reference_t<const foo&, bar&&>, bar&>);
+    static_assert(std::is_same_v<pol::
+        common_reference_t<int, char>, int>);
     return 0;
 }
