36b91cc32f7c069e4f9cecf6188f682799d29722
Polaris1949 <Polaris1949@126.com>
2018-10-29 22:06:16 +0800

    Update tree chain draft

--- todo/tree-chain-0.cpp
+++ todo/tree-chain-0.cpp
@@ -16,6 +16,9 @@ private:
 
 public:
     tree_chain_node(const value_type& __x);
+
+    value_type& mark();
+    const value_type& mark() const;
 };
 
 template<typename _Tp>
@@ -31,6 +34,16 @@ private:
 
 public:
     tree_chain() = default;
+
+    template<typename _Seq>
+    void init(const _Seq& __data);
+
+    void construct(size_type __b, size_type __e);
+    void push_down(node_type* __p, size_type __len);
+    value_type inquire(size_type __b, size_type __e);
+    void modify(size_type __b, size_type __e, const value_type& __k);
+    value_type inquire_child(size_type __b, size_type __e);
+    void modify_child(size_type __b, size_type __e, const value_type& __k);
 };
 
 }
