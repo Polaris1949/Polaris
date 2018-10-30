8644999c87e6faaff1c05b9b65411d8a6b86c54b
Polaris1949 <Polaris1949@126.com>
2018-10-30 19:29:34 +0800

    Update tree array implementation

--- todo/impl/bits/treearr.h
+++ todo/impl/bits/treearr.h
@@ -4,6 +4,7 @@
 namespace polaris
 {
 
+// TODO: More general base?
 class tree_array_base
 {
 public:
@@ -13,6 +14,41 @@ protected:
     static size_type _S_lowbit(size_type __x);
 };
 
+// TODO: More
+struct tree_array_mode
+{
+    // modify point & inquire range
+    struct mpir
+    {
+        template<typename _Tp>
+        using default_calc = plus_eq<_Tp>;
+    };
+
+    // modify range & inquire point
+    struct mrip
+    {
+        template<typename _Tp>
+        using default_calc = plus_eq<_Tp>;
+    };
+
+    // modify range & inquire range
+    struct mrir
+    {
+        template<typename _Tp>
+        using default_calc = plus_eq<_Tp>;
+    };
+
+    // TODO: modify point & inquire min/max value in range
+    struct mpim
+    {
+        template<typename _Tp>
+        using default_calc = max_f<_Tp>;
+    };
+};
+
+// TODO: template<_Tp, _Mode, _Cont, _Func>
+// template<typename _Tp, typename _Mode, typename _Cont,
+//     typename _Func = typename _Mode::default_calc<_Tp>>
 template<typename _Tp, typename _Mode>
 class tree_array
 {
@@ -20,6 +56,7 @@ public:
     static_assert(false_v<_Tp>, "no such mode");
 };
 
+// TODO: 80%
 template<typename _Tp>
 class tree_array<_Tp, tree_array_mode::mpir>
     : public tree_array_base
@@ -50,12 +87,86 @@ public:
     const value_type& at(size_type __pos) const;
     const value_type& operator[] (size_type __pos) const;
 
+    // TODO: Different modification operations
     void modify(size_type __x, const value_type& __k);
 
     value_type inquire(size_type __x, size_type __y);
 
 private:
     value_type _M_prefix(size_type __x);
+};
+
+// TODO: 80%
+template<typename _Tp>
+class tree_array<_Tp, tree_array_mode::mrip>
+    : public tree_array_base
+{
+public:
+    using value_type = _Tp;
+    using container_type = std::vector<_Tp>;
+    using size_type = std::size_t;
+
+private:
+    container_type _M_delta;
+
+public:
+    tree_array() = default;
+
+    template<typename _Seq>
+    tree_array(const _Seq& __data);
+
+    template<typename _Seq>
+    void init(const _Seq& __data);
+
+    bool empty() const;
+    void clear();
+
+    size_type size() const;
+    void resize(size_type __s);
+
+    const value_type& at(size_type __pos) const;
+    const value_type& operator[] (size_type __pos) const;
+
+    // TODO: Different modification operations
+    void modify(size_type __x, size_type __y, const value_type& __k);
+
+    value_type inquire(size_type __x);
+};
+
+// TODO: 0%
+template<typename _Tp>
+class tree_array<_Tp, tree_array_mode::mrir>
+    : public tree_array_base
+{
+public:
+    using value_type = _Tp;
+    using container_type = std::vector<_Tp>;
+    using size_type = std::size_t;
+
+private:
+    container_type _M_tree;
+
+public:
+    // TODO: Best implementation?
+    static_assert(false_v<_Tp>, "unimplemented");
+};
+
+// TODO: 0%
+template<typename _Tp>
+class tree_array<_Tp, tree_array_mode::mpim>
+    : public tree_array_base
+{
+public:
+    using value_type = _Tp;
+    using container_type = std::vector<_Tp>;
+    using size_type = std::size_t;
+
+private:
+    container_type _M_tree;
+
+public:
+    // TODO: Best implementation?
+    static_assert(false_v<_Tp>, "unimplemented");
 };
 
 }
--- todo/impl/bits/treearr.tcc
+++ todo/impl/bits/treearr.tcc
@@ -98,6 +98,91 @@ _M_prefix(size_type __x)
     return __res;
 }
 
+template<typename _Tp>
+template<typename _Seq>
+tree_array<_Tp, tree_array_mode::mrip>::
+tree_array(const _Seq& __data)
+{ this->init(__data); }
+
+template<typename _Tp>
+template<typename _Seq>
+void
+tree_array<_Tp, tree_array_mode::mrip>::
+init(const _Seq& __data)
+{
+    this->clear();
+    this->resize(__data.size());
+
+    for (size_type __i{}; __i < __data.size(); ++__i)
+        this->modify(__i, __i + 1, __data[__i]);
+}
+
+template<typename _Tp>
+inline bool
+tree_array<_Tp, tree_array_mode::mrip>::
+empty() const
+{ return this->_M_delta.empty(); }
+
+template<typename _Tp>
+inline void
+tree_array<_Tp, tree_array_mode::mrip>::
+clear()
+{ this->_M_delta.clear(); }
+
+template<typename _Tp>
+inline std::size_t
+tree_array<_Tp, tree_array_mode::mrip>::
+size() const
+{ return this->_M_delta.size(); }
+
+template<typename _Tp>
+inline void
+tree_array<_Tp, tree_array_mode::mrip>::
+resize(size_type __s)
+{ this->_M_delta.resize(__s); }
+
+template<typename _Tp>
+const _Tp&
+tree_array<_Tp, tree_array_mode::mrip>::
+at(size_type __pos) const
+{ return this->_M_delta.at(__pos); }
+
+template<typename _Tp>
+const _Tp&
+tree_array<_Tp, tree_array_mode::mrip>::
+operator[] (size_type __pos) const
+{ return this->_M_delta[__pos]; }
+
+template<typename _Tp>
+void
+tree_array<_Tp, tree_array_mode::mrip>::
+modify(size_type __x, size_type __y, const value_type& __k)
+{
+    for (size_type __i{__x}; __i < this->size(); __i += _S_lowbit(__i))
+        this->_M_delta[__i] += __k;
+    for (size_type __i{__y}; __i < this->size(); __i += _S_lowbit(__i))
+        this->_M_delta[__i] -= __k;
+}
+
+template<typename _Tp>
+_Tp
+tree_array<_Tp, tree_array_mode::mrip>::
+inquire(size_type __x)
+{
+    value_type __res{};
+    size_type __i{__x};
+    size_type __j{__x + 1};
+
+    while (__i < __j)
+    {
+        __res += this->_M_delta[__i];
+        __j = __i;
+        __i -= _S_lowbit(__i);
+    }
+
+    return __res;
+}
+
 }
 
 #endif /* _POL_TREEARR_TCC */
