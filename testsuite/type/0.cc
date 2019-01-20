98cf494fef973af898ab1ea783b8cd4b35c04f8b
Polaris1949 <Polaris1949@126.com>
2019-01-20 19:08:59 +0800

    Update type

--- testsuite/type/0.cc
+++ testsuite/type/0.cc
@@ -9,12 +9,14 @@ struct foo<_Tp, true> : public pol::fundamental_type<_Tp> {};
 
 struct bar
 {
+    bar() = default;
+    bar(int) {}
     bar& operator += (const bar&)
     { std::cout << "bar\n"; return *this; }
 };
 
 int main()
 {
-    foo<bar> x; x+=x;
-    foo<int> y;
+    foo<bar> x{1}; x+=x;
+    foo<int> y{2}; y+=y; std::cout << y.data() << '\n';
 }
