b768a78e9a5e8de7a1854dbe7c59755195f91030
Polaris1949 <Polaris1949@126.com>
2019-01-22 22:43:46 +0800

    Update type traits

--- testsuite/concepts/traits.cc
+++ testsuite/concepts/traits.cc
@@ -1,6 +1,11 @@
 #include <polaris/type_traits>
+#include <functional>
+#include <iostream>
 
 int main()
 {
+	std::cout << pol::is_operable_v<std::plus<>, int, int> << '\n';
+	std::cout << pol::is_plusable_v<int> << '\n';
+	std::cout << pol::is_plusable_with_v<int, short> << '\n';
 	return 0;
 }
