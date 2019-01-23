63d1e5d65c969cfb90bbd74378fc40c216e6bf19
Polaris1949 <Polaris1949@126.com>
2019-01-23 18:38:18 +0800

    Update type traits

--- testsuite/concepts/traits.cc
+++ testsuite/concepts/traits.cc
@@ -7,5 +7,8 @@ int main()
 	std::cout << pol::is_operable_v<std::plus<>, int, int> << '\n';
 	std::cout << pol::is_plusable_v<int> << '\n';
 	std::cout << pol::is_plusable_with_v<int, short> << '\n';
+	std::cout << pol::is_plus_assignable_v<int> << '\n';
+	std::cout << pol::is_equality_comparable_v<int> << '\n';
+	std::cout << pol::is_less_than_comparable_v<int> << '\n';
 	return 0;
 }
